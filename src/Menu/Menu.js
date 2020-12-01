569e8bc5d4daf74113dd7960ba9c66a5d62ccf73
janhenrikfock <jan.henrik.fock@googlemail.com>
2020-12-02 00:02:32 +0100

    Reorganize datastructure using reduce / Use object.entries method in menu.js

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -5,14 +5,18 @@ import { StyledFoodGrid, StyledFood, StyledFoodLabel } from './FoodGrid'
 export default function Menu() {
   return (
     <MenuStyled>
-      <h1> Menu </h1>
-      <StyledFoodGrid>
-        {foods.map((food) => (
-          <StyledFood img={food.img}>
-            <StyledFoodLabel>{food.name}</StyledFoodLabel>
-          </StyledFood>
-        ))}
-      </StyledFoodGrid>
+      {Object.entries(foods).map(([sectionName, foods]) => (
+        <>
+          <h1> {sectionName} </h1>
+          <StyledFoodGrid>
+            {foods.map((food) => (
+              <StyledFood img={food.img}>
+                <StyledFoodLabel>{food.name}</StyledFoodLabel>
+              </StyledFood>
+            ))}
+          </StyledFoodGrid>
+        </>
+      ))}
     </MenuStyled>
   )
 }
