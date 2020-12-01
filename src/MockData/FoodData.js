26de1b9a1ea00b9bbc66cf4ee0e362e37bfdff1b
janhenrikfock <jan.henrik.fock@googlemail.com>
2020-12-01 22:52:38 +0100

    Add all foods

--- src/MockData/FoodData.js
+++ src/MockData/FoodData.js
@@ -7,4 +7,28 @@ export const foods = [
     name: 'Pepperoni Pizza',
     img: '/img/pizza2.jpeg',
   },
+  {
+    name: 'Chicken Pizza',
+    img: '/img/chicken-pizza.jpeg',
+  },
+  {
+    name: 'Veggie Pizza',
+    img: '/img/healthy-pizza.jpeg',
+  },
+  {
+    name: 'Burger',
+    img: '/img/burger.jpeg',
+  },
+  {
+    name: 'Sandwich',
+    img: '/img/sandwich.jpeg',
+  },
+  {
+    name: 'Gyro',
+    img: '/img/gyro.jpeg',
+  },
+  {
+    name: 'Fries',
+    img: '/img/fries.jpeg',
+  },
 ]
